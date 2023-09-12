1940b6b68247979955c67d723013cd735c24de1c
Essillore <riikka.pehkonen@me.com>
2023-09-12 13:37:32 +0300

    Commit 12.9.23_0
    Added intellect and superSpeed variables to cubescript

--- Assets/CubeScript.cs
+++ Assets/CubeScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CubeScript : MonoBehaviour
 {
     public float speed = 2f;
+    public float superSpeed = 4f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,5 +17,10 @@ public class CubeScript : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector3.right * speed * Time.deltaTime);
+        float intellect = 2;
+        if (intellect>3)
+            {
+            speed = superSpeed;
+        }
     }
 }
