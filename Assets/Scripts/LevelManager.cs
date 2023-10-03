0e3805b8de0d768eace292334a8fbef6da0c29d6
Essillore <riikka.pehkonen@me.com>
2023-10-03 07:22:13 +0300

    Commit031023_3 LevelManager and DontDestroyOnLoad scripts placed
    to do: Get Cinemachine-camera to find target when loaded to scene

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            SceneManager.LoadScene(3);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            if (SceneManager.GetActiveScene().buildIndex == 3)
+            {
+                SceneManager.LoadScene(1);
+            }
+            else
+            {
+                SceneManager.LoadScene(3);
+            }
+
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+}
